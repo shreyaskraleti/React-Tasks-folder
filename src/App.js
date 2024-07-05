8c8d4cbbb82fba33a29ecdea56ecebf65cc2e843
Shreyas <shreyas.kraleti2@gmail.com>
2024-07-05 18:38:19 +0530

    login form files added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Loginform from './Components/Loginform';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Loginform />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Loginform.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import "./Loginform.css";
+
+function Loginform() {
+        return (
+            <>
+               
+        
+               <form className="Loginform" >
+        
+               <h1>Create a new account</h1> 
+               <h4>It's quick and easy.</h4>
+        
+               <div className="form-group">
+               <label>Email: </label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Mobile number or email address"
+                            // value={formData.email}
+                            // onChange={}
+                            required
+                        />
+                    </div>
+        
+                    <div className="form-group">
+                    <label>Password: </label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="New password"
+                            // value={formData.password}
+                            // onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+            <label>Date of birth: </label>
+            <input
+                type="date"
+                name="dateOfBirth"
+                // value={formData.dateOfBirth}
+                // onChange={handleChange}
+                required
+            />
+               </div>
+        
+               <div className="form-group">
+               <label>FirstName:  </label>
+                        <input
+                            type="text"
+                            name="firstName"
+                            placeholder="First name"
+                            // value={formData.firstName}
+                            // onChange={handleChange}
+                            required
+                        />
+                    </div>
+        
+                    <div className="form-group">
+                    <label>LastName:  </label>
+                        <input
+                            type="text"
+                            name="surname"
+                            placeholder="Surname"
+                            // value={formData.surname}
+                            // onChange={handleChange}
+                            required
+                        />
+                    </div>
+        
+                    <div className="form-group">
+                        <label>Gender</label>
+                        <div className="gender-options">
+                            <input
+                                type="radio"
+                                name="gender"
+                                // value="female"
+                                // onChange={handleChange}
+                            /> Female
+                            <input
+                                type="radio"
+                                name="gender"
+                                // value="male"
+                                // onChange={handleChange}
+                            /> Male
+                            <input
+                                type="radio"
+                                name="gender"
+                                // value="custom"
+                                // onChange={handleChange}
+                            /> Custom
+                        </div>
+                    </div>
+                    
+                    <button type="submit" className="submit-button">Sign Up</button>
+                    <p>Already have an account? <a href="https://mail.google.com/mail/u/0/#inbox">Login</a></p>
+                    
+                    </form>
+                    </>
+                    )
+                }
+                
+                export default Loginform;
